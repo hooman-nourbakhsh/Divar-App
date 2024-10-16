18c26416cb1b6b026ccb10284846765905d6e326
HoTaN <id4acc2014@protonmail.com>
2024-10-16 23:11:41 +0330

    feat: :sparkles: I Set Up React Query with Configuration and Devtools
    I installed `@tanstack/react-query` and `@tanstack/react-query-devtools` to manage server state within the project.
    Then, I created a `reactQuery.js` file in `src/configs/` to define default configuration options for React Query, such as disabling refetch on window focus and mount, setting the retry limit to 2, and specifying a `staleTime` of 60 seconds. In `App.jsx`, I initialized a `QueryClient` using these options and wrapped the application with `QueryClientProvider` to enable React Query.
    I also added `ReactQueryDevtools` to assist with debugging.

---
+++ src/configs/reactQuery.js
@@ -0,0 +1,10 @@
+const defaultOptions = {
+  queries: {
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+    retry: 2,
+    staleTime: 60 * 1000,
+  },
+};
+
+export default defaultOptions;

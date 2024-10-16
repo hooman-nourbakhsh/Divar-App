b9e21b8590b7ae46ac06fdb3fff3bb4926989a72
HoTaN <id4acc2014@protonmail.com>
2024-10-16 19:25:04 +0330

    feat: :sparkles: I Passed Props to CheckOtpForm and Implemented OTP Input Logic
    I updated the `AuthPage` component to pass the necessary props (`code`, `setCode`, `mobile`, and `setStep`) to the `CheckOtpForm` component. In `CheckOtpForm`, I implemented a form for entering the OTP code sent to the user's mobile number.
    The form displays the mobile number to which the OTP was sent and includes an input field for the OTP.
    The `setCode` function updates the `code` state as the user types.
    There are two buttons: one for submitting the OTP and another to allow the user to go back and change the mobile number, which sets the step back to 1.

--- src/components/templates/CheckOtpForm.jsx
+++ src/components/templates/CheckOtpForm.jsx
@@ -1,8 +1,22 @@
-
-function CheckOtpForm() {
+function CheckOtpForm({ code, setCode, mobile, setStep }) {
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
+  
   return (
-    <div>CheckOtpForm</div>
-  )
+    <form onSubmit={submitHandler}>
+      <p>تایید شماره موبایل</p>
+
+      <span>کد تایید به شماره موبایل {mobile} ارسال شده است</span>
+
+      <label htmlFor="input">کد تایید را وارد کنید</label>
+
+      <input type="text" id="input" placeholder="کد تایید" value={code} onChange={(e) => setCode(e.target.value)} />
+
+      <button type="submit">تایید</button>
+      <button onClick={() => setStep(1)}>تغییر شماره موبایل</button>
+    </form>
+  );
 }
 
-export default CheckOtpForm
+export default CheckOtpForm;

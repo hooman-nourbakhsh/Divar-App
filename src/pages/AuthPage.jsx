0ab95299a2be34c18bf4686010ce42af6a61fe17
HoTaN <id4acc2014@protonmail.com>
2024-10-16 18:46:07 +0330

    feat: :sparkles: I Passed Props to SendOtpForm and Implemented Form Logic
    I updated the `AuthPage` component to pass necessary props (`setStep`, `mobile`, and `setMobile`) to the `SendOtpForm` component.
    In the `SendOtpForm` component, I added a form with input for the mobile number, which is controlled by the `mobile` state.
    The `setMobile` function updates the state on input change.
    I also created a `submitHandler` function to prevent the default form submission behavior.
    The form displays a message explaining the mobile number input and a button to send the OTP.

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -9,7 +9,7 @@ function AuthPage() {
 
   return (
     <div>
-      {step === 1 && <SendOtpForm />}
+      {step === 1 && <SendOtpForm setStep={setStep} mobile={mobile} setMobile={setMobile} />}
       {step === 2 && <CheckOtpForm />}
     </div>
   );

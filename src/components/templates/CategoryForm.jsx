649e275cebc04f8af4ece1d55916c62c83f3348c
HoTaN <id4acc2014@protonmail.com>
2024-10-17 19:12:17 +0330

    feat: :sparkles: I added category submission with react-query to CategoryForm.
    I added category submission to the `CategoryForm` using `react-query`.
    Created `addCategory` in `admin.js` for posting category data.
    Integrated `useMutation` in `CategoryForm` to handle submissions.
    Added form validation to ensure all fields are filled.
    Displayed loading, success, and error states in the form UI.
    Disabled the submit button during the API call.
    These changes improve category management functionality for admin users.

--- src/components/templates/CategoryForm.jsx
+++ src/components/templates/CategoryForm.jsx
@@ -1,9 +1,13 @@
+import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
+import { addCategory } from "services/admin";
 
 import styles from "styles/CategoryForm.module.css";
 
 function CategoryForm() {
   const [form, setForm] = useState({ name: "", slug: "", icon: "" });
+
+  const { mutate, isLoading, error, data } = useMutation(addCategory);
 
   const changeHandler = (event) => {
     setForm({ ...form, [event.target.name]: event.target.value });
@@ -11,19 +15,26 @@ function CategoryForm() {
 
   const submitHandler = (event) => {
     event.preventDefault();
+
+    if (!form.name || !form.slug || !form.icon) return;
+
+    mutate(form);
   };
 
   return (
     <form onChange={changeHandler} onSubmit={submitHandler} className={styles.form}>
       <h3>دسته بندی جدید</h3>
-      <p></p>
+      {!!error && <p>مشکلی پیش آمده است - {error.message}</p>}
+      {data?.status === 201 && <p className={styles.success}>دسته بندی با موفقیت ایجاد شد</p>}
       <label htmlFor="name">اسم دسته بندی</label>
       <input type="text" name="name" id="name" />
       <label htmlFor="slug">اسلاگ</label>
       <input type="text" name="slug" id="slug" />
       <label htmlFor="icon">آیکون</label>
       <input type="text" name="icon" id="icon" />
-      <button type="submit">ایجاد</button>
+      <button type="submit" disabled={isLoading}>
+        افزودن
+      </button>
     </form>
   );
 }

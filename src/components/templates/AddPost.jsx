0accd03d3f99133894dafcafe2ad6c30498dda62
HoTaN <id4acc2014@protonmail.com>
2024-10-17 23:55:07 +0330

    feat: :sparkles: I updated the AddPost component to handle form input changes and log the form data
    I enhanced the AddPost component to manage the form state using the `useState` hook.
    The form now captures inputs for title, content, amount, city, category, and an image file.
    I implemented an `onChange` handler to update the form state dynamically.
    For image uploads, the handler processes file input separately from other fields.
    When the form is submitted, the current form data is logged to the console for verification.

--- src/components/templates/AddPost.jsx
+++ src/components/templates/AddPost.jsx
@@ -1,32 +1,59 @@
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 import { getCategory } from "services/admin";
 
 function AddPost() {
+  const [form, setForm] = useState({
+    title: "",
+    content: "",
+    amount: null,
+    city: "",
+    category: "",
+    images: null,
+  });
+
   const { data } = useQuery(["get-categories"], getCategory);
+
+  const changeHandler = (event) => {
+    const name = event.target.name;
+    if (name !== "images") {
+      setForm({ ...form, [name]: event.target.value });
+    } else {
+      setForm({ ...form, [name]: event.target.files[0] });
+    }
+  };
   
   const addHandler = (event) => {
     event.preventDefault();
+    console.log(form);
   };
 
   return (
-    <form>
+    <form onChange={changeHandler}>
       <h3>افزودن آگهی</h3>
+
       <label htmlFor="title">عنوان</label>
       <input type="text" name="title" id="title" />
+
       <label htmlFor="content">توضیحات</label>
       <textarea name="content" id="content" />
+
       <label htmlFor="amount">قیمت</label>
       <input type="text" name="amount" id="amount" />
+
       <label htmlFor="city">شهر</label>
       <input type="text" name="city" id="city" />
+
       <label htmlFor="category">دسته بندی</label>
       <select name="category" id="category">
         {data?.data.map((category) => (
           <option key={category._id}>{category.name}</option>
         ))}
       </select>
+
       <label htmlFor="images"></label>
       <input type="file" name="images" id="images" />
+
       <button onClick={addHandler}>افزودن</button>
     </form>
   );

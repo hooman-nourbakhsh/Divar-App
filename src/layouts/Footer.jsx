035cc6c9bd14419b3be3e965cbc4ebdfbcb46ef2
HoTaN <id4acc2014@protonmail.com>
2024-10-17 13:14:25 +0330

    feat: :sparkles: I Developed Layout, Header, and Footer Components with CSS Modules
    I created the `Header`, `Footer`, and `Layout` components to structure the overall layout of the application.
    The `Header.jsx` contains navigation links, including the logo, location, and user profile, while the `Footer.jsx` displays a copyright message.
    Additionally, I developed the `Layout.jsx` component to wrap around the content of the pages, encapsulating the header and footer.
    All components are styled using CSS modules (`Header.module.css`, `Footer.module.css`, and `Layout.module.css`), ensuring scoped and maintainable styles across the application.
    These updates significantly enhance the overall structure and design consistency of the project.

---
+++ src/layouts/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "styles/Footer.module.css";
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>تمامی حقوق مادی و معنوی این سایت متعلق به دیوار می باشد</p>
+    </footer>
+  );
+}
+
+export default Footer;

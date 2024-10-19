9810d789ae23dad434eaa7699e2ab23aeef26e18
HoTaN <id4acc2014@protonmail.com>
2024-10-19 11:16:20 +0330

    feat: :sparkles: I implemented number formatting and applied styles to the PostList component
    I added a utility file `numbers.js` in the `utils` folder to handle the conversion between Persian and English numerals using functions `e2p`, `p2e`, and `sp`.
    The `sp` function formats numbers with commas and converts them to Persian digits.
    In the `PostList` component, I utilized `sp` to format the post's amount in Persian digits and with commas for easier readability.
    I also formatted the post creation date using `toLocaleDateString("fa-IR")` for the Persian calendar.
    Additionally, I applied CSS module styles to the `PostList` component to enhance its visual layout.

--- src/components/templates/PostList.jsx
+++ src/components/templates/PostList.jsx
@@ -1,26 +1,29 @@
 import { useQuery } from "@tanstack/react-query";
 import Loader from "components/modules/Loader";
 import { getPosts } from "services/user";
+import { sp } from "utils/numbers";
+
+import styles from "styles/PostList.module.css";
 
 function PostList() {
   const { data, isLoading } = useQuery(["my-post-list"], getPosts);
   return (
-    <div>
+    <div className={styles.list}>
       {isLoading ? (
         <Loader />
       ) : (
         <>
           <h3>آگهی های شما</h3>
           {data.data.posts.map((post) => (
-            <div key={post._id}>
+            <div key={post._id} className={styles.post}>
               <img src={`${import.meta.env.VITE_BASE_URL}${post.images[0]}`} />
               <div>
                 <p>{post.options.title}</p>
                 <span>{post.options.content}</span>
               </div>
-              <div>
-                <p>{post.createdAt}</p>
-                <span>{post.amount} تومان</span>
+              <div className={styles.price}>
+                <p>{new Date(post.createdAt).toLocaleDateString("fa-IR")}</p>
+                <span>{sp(post.amount)} تومان</span>
               </div>
             </div>
           ))}
